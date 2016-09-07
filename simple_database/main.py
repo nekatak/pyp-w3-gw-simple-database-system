a173a0ee49b4f50907e4cb259e68c38b0072c731
nekatak <nekatak200@yahoo.gr>
2016-09-07 17:36:22 +0000

    first test passes!

--- simple_database/main.py
+++ simple_database/main.py
@@ -17,52 +17,59 @@ class Make_db(object):
             os.makedirs(BASE_DB_FILE_PATH + db_name)
 
     def show_tables(self):
+
         arr=[]
         path=BASE_DB_FILE_PATH+self.db_name
         for file in os.listdir(path):
-            if file.endswith(".json"):
-                arr.append(file)
-
+            arr.append(file)
         return arr
 
+
     def create_table(self, name, columns):
-        #arrOfTables = self.show_tables
-        #if name in arrOfTables:
-            #raise ValidationError("Database with name %s already exists.")%name
+        if type(name) != str:
+            raise TypeError('Invalid table name type.')
         table = Table(self.db_name, name, columns)
         setattr(self, name, table)
-        #return table
+
 
 class Table(object):
     def __init__(self,db_name, name, columns):
         self.db_name=db_name
         self.table_name=name
         self.columns={}
+        self.rows=0
         fout=open(BASE_DB_FILE_PATH + db_name + "/" + name, 'wt')
         self.columns['structure']=columns
         data = json.dumps(self.columns)
         fout.write(data)
         fout.close()
 
+
+
     def insert(self,*args):
+
         if len(self.columns["structure"])<len(args):
             raise ValidationError("Invalid amount of fields.")
         fout = open(BASE_DB_FILE_PATH + self.db_name + "/" + self.table_name, 'at')
         ndict={}
         i=0
         for item in self.columns["structure"]:
-            name=item["name"]
-            if type( args[i]) is not eval(item["type"]):
+            name = item["name"]
+            if type(args[i]) is not eval(item["type"]):
                 raise ValidationError('Invalid type of field %s: Given %s"str", expected %s"date"')%(name, type(args[i]), item["type"])
-            ndict[name]=args[i]
+            val = args[i]
+            if type(args[i]) is date:
+                val=str(args[i])
+            ndict[name]=val
             i+=1
         data = json.dumps(ndict)
+        self.rows+=1
         fout.write(data)
         fout.close()
 
+
     def count(self):
-        with open(BASE_DB_FILE_PATH + db_name + "/" + table_name, 'rt'):
-            data = json.load
+        return self.rows
 
 
     def describe(self):
